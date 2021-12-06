addb4b2805a6d803f10b5b4988160708a9c4c701
Rafael Sana Montevechio <rafael_sana@hotmail.com>
2021-12-06 09:02:30 -0300

    feat: :sparkles: added the document file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
 import type { NextPage } from 'next';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
-  return <h1>Hello</h1>;
+  return (
+    <>
+      <Head>
+        <title>World trip</title>
+      </Head>
+      <h1>Hello</h1>
+    </>
+  );
 };
 
 export default Home;
