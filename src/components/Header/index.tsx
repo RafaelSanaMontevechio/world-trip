4c0f9c978fe1ae513c80d5b2ebc5e9e5281e17f9
Rafael Sana Montevechio <rafael_sana@hotmail.com>
2021-12-20 11:36:15 -0300

    some changes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,30 +1,46 @@
-import { Flex, IconButton, Icon } from '@chakra-ui/react';
+import { Flex, IconButton, Icon, Grid, Image } from '@chakra-ui/react';
 
-import Image from 'next/image';
+// import Image from 'next/image';
 
 import { MdOutlineArrowBackIosNew } from 'react-icons/md';
 
 import WordTripLogo from '../../assets/world-trip_logo.svg';
 
 interface HeaderProps {
-  goBackPath: string;
+  goBackPath?: string;
 }
 
 const Header = ({ goBackPath }: HeaderProps) => {
   return (
     <Flex
+      bg="white"
       as="header"
       w="100%"
-      h="20"
-      maxWidth={1440}
       mx="auto"
-      mt="2"
-      px="6"
+      h={['40px', '90px']}
+      px="1rem"
       align="center"
       justify="center"
-      borderBottom="1px solid #e6e6e6"
-      position="absolute"
     >
+      <Grid
+        h="100%"
+        mx="auto"
+        w="100%"
+        maxWidth="1160px"
+        align="center"
+        justifyContent="center"
+        templateColumns="repeat(3, 1fr)"
+      >
+        <Image
+          w={['81px', '184px']}
+          mt="6"
+          src="/world-trip_logo.svg"
+          alt="Word trip"
+          justifySelf="center"
+          gridColumn={2}
+        />
+      </Grid>
+
       {/*TO DO corrigir header*/}
       {/* <Flex> */}
       {/* {goBackPath && ( */}
@@ -37,9 +53,7 @@ const Header = ({ goBackPath }: HeaderProps) => {
       /> */}
       {/* )} */}
       {/* </Flex> */}
-      <Flex>
-        <Image src={WordTripLogo} alt="Word trip" />
-      </Flex>
+      <Flex></Flex>
     </Flex>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,27 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
 
+import { Flex } from '@chakra-ui/react';
+
+import Title from '../components/Title';
 import Header from '../components/Header';
+import Banner from '../components/Banner';
+import Carousel from '../components/Carousel';
+import TravelTypes from '../components/TravelTypes';
+import CallToAction from '../components/CallToAction';
+import Divider from '../components/Divider';
 
 const Home: NextPage = () => {
   return (
     <>
-      <Head>
-        <title>World trip</title>
-      </Head>
-      <Header />
+      <Title />
+      <Flex w="100%" direction="column">
+        <Header />
+        <Banner />
+        <TravelTypes />
+        <Divider />
+        <CallToAction />
+      </Flex>
+      <Carousel />
     </>
   );
 };
