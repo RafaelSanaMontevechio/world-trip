d803269a36a9680d6f7caffe6c75911f57b8e4c1
Rafael Sana Montevechio <rafael_sana@hotmail.com>
2021-12-20 11:32:33 -0300

    Call to action

---
+++ src/components/CallToAction/index.tsx
@@ -0,0 +1,16 @@
+import { Heading } from '@chakra-ui/react';
+
+const CallToAction = () => {
+  return (
+    <Heading
+      textAlign="center"
+      fontWeight="500"
+      mb={['5', '14']}
+      fontSize={['lg', '2xl', '3xl', '4xl']}
+    >
+      Vamos nessa? <br /> então escolha seu continente
+    </Heading>
+  );
+};
+
+export default CallToAction;
